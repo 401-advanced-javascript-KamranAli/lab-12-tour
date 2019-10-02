29027261758e059448054836773f47ece7c959e1
Kamran Ali <kgali21@gmail.com>
2019-10-02 13:24:32 -0700

    fixed up file structure, confused on what was needed in lab

--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -1,52 +1,84 @@
+// jest.mock('../../lib/services/maps-api');
 const request = require('../request');
 const db = require('../db');
+const { matchMongoId } = require('../match-helpers');
+// const getLocation = require('../../lib/services/maps-api');
 
-describe('stops api', () => {
+// getLocation.mockResolvedValue({
+//   latitude: 38,
+//   longitude: -130
+// });
+
+describe('locations api', () => {
   beforeEach(() => {
-    return Promise.all([
-      db.dropCollection('stops'),
-      db.dropCollection('tours')
-    ]);
+    return db.dropCollection('locations');
   });
-
   const firstTours = {
-    title: 'italy trip',
+    title: 'italy trip',
     activities: [
       {
-        activity1: 'Tower of Pisa',
-        activity2: 'The Vatican',
-        activity3: 'Tower of Pizza'
+        activity1: 'Tower of Pisa',
+        activity2: 'The Vatican',
+        activity3: 'Tower of Pizza'
       }
-    ]
+    ],
+    address: '97008'
   };
 
-  const testStop = [{}];
-
-  function postStop(stops) {
+  function postTours(tours) {
     return request
-      .post('/api/stops')
-      .send(firstTours)
+      .post('/api/tours')
+      .send(tours)
       .expect(200)
-      .then(({ body }) => {
-        stops[0] = body._id;
-        return request
-          .post('/api/stops')
-          .send(testStop)
-          .expect(200);
-      })
       .then(({ body }) => body);
   }
 
-  it('posts a stop ', () => {
-    return postStop(testStop).then(stop => {
-      expect(stop).toMatchInlineSnapshot(
-        {
-          _id: expect.any(String),
-          stops: [expect.any(Object)]
-        },
+  it('add a tours, gets geo tours', () => {
+    return postTours(firstTours).then(tour => {
+      expect(tour).toMatchInlineSnapshot(
+        matchMongoId,
 
+        `
+        Object {
+          "__v": 0,
+          "_id": StringMatching /\\^\\[a-f\\\\d\\]\\{24\\}\\$/i,
+          "activities": Array [
+            Object {
+              "_id": "5d9505af0dae18d9db08556d",
+              "activity1": "Tower of Pisa",
+              "activity2": "The Vatican",
+              "activity3": "Tower of Pizza",
+            },
+          ],
+          "address": 97008,
+          "launchDate": "2019-10-02T20:16:47.749Z",
+          "stops": Array [],
+          "title": "italy trip",
+        }
+      `
       );
     });
   });
 
+  // const stop1 = { title: 'Sicily' };
+
+  // function postTourWithStop(tour, stop) {
+  //   return postTours(tour).then(tour => {
+  //     return request
+  //       .post(`/api/tours/${tour._id}/stops`)
+  //       .send(stop)
+  //       .expect(200)
+  //       .then(({ body }) => [tour, body]);
+  //   });
+  // }
+
+//   it('adds a stop to the tour', () => {
+//     return postTourWithStop(firstTours, stop1).then(([, stops]) => {
+//       expect(stops[0]).toEqual({
+//         ...matchMongoId,
+//         ...stop1,
+//         date: expect.any(String)
+//       });
+//     });
+//   });
 });
